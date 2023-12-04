2ff950c58a36a45dd546d0b798fa03a44476fb38
sohith12 <sohith12@gmail.com>
2023-12-04 14:28:45 -0600

    removed extra lines

--- code.py
+++ code.py
@@ -1,16 +1,11 @@
-It looks like you're asking for a Python script that uses the Duo SDK to monitor Telephony Credits for users who cannot use the Duo app on smartphones and instead rely on Duo Telephony (phone calls or SMS messages). The script should alert when the Telephony Credits are running low, prompting the purchase of more credits to avoid disruptions. Here's an outline of how you can approach this task:
 
-### Duo Script to Monitor Telephony Credits
-
-#### Requirements:
-
-1. **Duo SDK for Python:**
+ **Duo SDK for Python:**
    - Make sure you have the Duo SDK for Python installed. You can typically install it using a package manager like pip:
      ```bash
      pip install duo_client
      ```
 
-2. **API Keys:**
+ **API Keys:**
    - Retrieve the "UTD ISO Telephony Credits Monitor API Keys" from the "Shared-Engineering Team – Students" LastPass folder.
 
 #### Python Script:
